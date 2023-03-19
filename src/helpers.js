d47c5f83863bc7452b5288a0e0b714dad85031ca
avmirolyubov <avmirolyubov@avito.ru>
2023-03-19 13:31:07 +0300

    master: small fixes;

--- src/helpers.js
+++ src/helpers.js
@@ -1,17 +1,17 @@
 import readlineSync from 'readline-sync';
 import getUsername from './cli.js';
 
-export function welcomeMessage() {
+export const welcomeMessage = () => {
   console.log('Welcome to the Brain Games!');
   const username = getUsername();
   console.log(`Hello, ${username}`);
   return username;
-}
+};
 
-export function generateRandomNumber(
+export const generateRandomNumber = (
   minValueOfGeneratedNumber = 0,
   maxValueOfGeneratedNumber = 20,
-) {
+) => {
   if (maxValueOfGeneratedNumber < minValueOfGeneratedNumber) {
     throw new Error(
       'Max value of number can not be less than min value',
@@ -22,13 +22,11 @@ export function generateRandomNumber(
       * (maxValueOfGeneratedNumber - minValueOfGeneratedNumber)
       + minValueOfGeneratedNumber,
   );
-}
+};
 
-export function askUserAQuestion(questionCondition) {
-  return readlineSync.question(`Question: ${questionCondition}\nYour answer: `);
-}
+export const askUserAQuestion = (questionCondition) => readlineSync.question(`Question: ${questionCondition}\nYour answer: `);
 
-export function generateWrongAnswerMessage(answer, correctAnswer, username) {
+export const generateWrongAnswerMessage = (answer, correctAnswer, username) => {
   const looseMessage = `Let's try again, ${username}!`;
   return `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\n${looseMessage}`;
-}
+};
